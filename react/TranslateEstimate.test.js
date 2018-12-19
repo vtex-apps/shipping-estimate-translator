3abb71ef917c83a1316f25bbdacfdccd0b04ad33
Fernando de Almeida Coelho <nandoacoelho@gmail.com>
2018-12-19 16:38:48 -0200

    Update unit tests configuration

--- react/TranslateEstimate.test.js
+++ react/TranslateEstimate.test.js
@@ -1,5 +1,10 @@
+import TranslateEstimate from './TranslateEstimate'
+import TestRenderer from 'react-test-renderer'
+
 describe('TranslateEstimate', () => {
   it('return null if does not pass shippingEstimate', () => {
+    const wrapper = TestRenderer.create(TranslateEstimate)
+    console.log('wrapper', wrapper)
     expect(true).toBe(true)
   })
 })
