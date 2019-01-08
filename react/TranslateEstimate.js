470eead9fd78c35ed90733486c1a8aeb75784516
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-01-08 12:01:06 -0200

    Refactor component code
    Fix bug where the component would update it's state until React stoped it and raised an exception.
    
    Refactor `TranslateEstimate` from a `class` to a functional component.

--- react/TranslateEstimate.js
+++ react/TranslateEstimate.js
@@ -2,50 +2,30 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { injectIntl, intlShape } from 'react-intl'
 
-class TranslateEstimate extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      id: this.getTranslateId(props.shippingEstimate, props.isPickup),
-      timeAmount: this.getTimeAmount(props.shippingEstimate),
-    }
-  }
-
-  componentDidUpdate() {
-    const { shippingEstimate, isPickup } = this.props
-
-    this.setState({
-      id: this.getTranslateId(shippingEstimate, isPickup),
-      timeAmount: this.getTimeAmount(shippingEstimate),
-    })
-  }
-
-  getTranslateId(shippingEstimate, isPickup) {
-    const shippingEstimateString =
+const getTranslateId = (shippingEstimate, isPickup) => {
+  const shippingEstimateString =
       shippingEstimate && shippingEstimate.split(/[0-9]+/)[1]
 
-    return (
-      shippingEstimate &&
+  return (
+    shippingEstimate &&
       shippingEstimateString &&
       `shippingEstimate${isPickup ? 'Pickup' : ''}-${
         shippingEstimate.split(/[0-9]+/)[1]
       }`
-    )
-  }
+  )
+}
 
-  getTimeAmount(shippingEstimate) {
-    return shippingEstimate && shippingEstimate.split(/\D+/)[0]
-  }
+const getTimeAmount = (shippingEstimate) => {
+  return shippingEstimate && shippingEstimate.split(/\D+/)[0]
+}
 
-  render() {
-    const { intl } = this.props
-    const { id, timeAmount } = this.state
+const TranslateEstimate = ({ intl, shippingEstimate, isPickup }) => {
+  const id = getTranslateId(shippingEstimate, isPickup)
+  const timeAmount = getTimeAmount(shippingEstimate)
 
-    return id && timeAmount && intl
-      ? intl.formatMessage({ id }, { timeAmount })
-      : ''
-  }
+  return id && timeAmount && intl
+    ? intl.formatMessage({ id }, { timeAmount })
+    : ''
 }
 
 TranslateEstimate.defaultProps = {
