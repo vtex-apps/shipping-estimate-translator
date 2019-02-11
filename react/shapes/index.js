03d496be48c1575a54e65280fd328171d9fa2cad
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-02-11 18:43:44 -0200

    Migrate to TypeScript and refactor repo structure
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- react/shapes/index.js
+++
@@ -1,6 +0,0 @@
-import PropTypes from 'prop-types'
-
-export const deliveryWindowShape = PropTypes.shape({
-  startDateUtc: PropTypes.string,
-  endDateUtc: PropTypes.string,
-})
--- react/test-utils/index.tsx
+++ react/test-utils/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { IntlProvider } from 'react-intl'
 import { render } from 'react-testing-library'
-import messages from '../messages/pt.json'
+import messages from '../../messages/pt.json'
 
-const renderWithIntl = node => {
+const renderWithIntl = (node: ReactNode) => {
   return render(
     <IntlProvider locale="pt" messages={messages}>
       {node}
